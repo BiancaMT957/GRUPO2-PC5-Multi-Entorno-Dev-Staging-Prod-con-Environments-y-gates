f0978a4436f56e0e98a36654782a92849828453b
LuisCalapuja <lcalapujaa@uni.com>
2025-12-03 14:40:58 -0500

    refactor: app y routers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
+import os
 from fastapi import FastAPI
 from routers import services
 
+# lee entornos desde var entornos
+APP_ENV = os.getenv("APP_ENV", "unknown")
 app = FastAPI(title="Service Catalog API")
 
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {"status": "ok",
+            "env": APP_ENV # indicador del entorno
+    }
 
 
 app.include_router(services.router)
